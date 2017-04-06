cca325dc29a8bd115cb7b468b3d2a3b5457f6d20
m <martin.tierney86@hotmail.co.uk>
2017-04-06 22:08:50 +0100

    bolt added

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,11 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
+public class Boundary
+{
+    public float xMin, xMax, zMin, zMax;
+}
+
 public class PlayerController : MonoBehaviour {
 
     public float speed;
+    public Boundary boundary;
+    public float tilt;
+    public float fireRate;
+    public GameObject Bolt;
+    public Transform ShotSpawn;
 
     private Rigidbody rb;
+    private float nextFire;
 
     // Use this for initialization
     void Start () {
@@ -15,8 +27,12 @@ public class PlayerController : MonoBehaviour {
 
     // Update is called once per frame
     void Update () {
-		
-	}
+        if (Input.GetButton("Fire1") && Time.time > nextFire)
+        {
+            nextFire = Time.time + fireRate;
+            Instantiate(Bolt, ShotSpawn.position, ShotSpawn.rotation);
+        }
+    }
 
     void FixedUpdate()
     {
@@ -25,5 +41,14 @@ public class PlayerController : MonoBehaviour {
 
         Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
         rb.velocity = movement * speed;
+
+        rb.position = new Vector3
+            (
+                Mathf.Clamp(rb.position.x, boundary.xMin, boundary.xMax),
+                0.0f,
+                Mathf.Clamp(rb.position.z, boundary.zMin, boundary.zMax)
+            );
+
+        rb.rotation = Quaternion.Euler(0.0f, 0.0f, rb.velocity.x * -tilt);
     }
 }
